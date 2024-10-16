1062b3f33d1867c9b54169af8db3eb09f977ef5a
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 20:07:48 +0800

    feat: adding new product in store

---
+++ product.py
@@ -0,0 +1,5 @@
+product_names = ["Laptop", "Smartphone", "Headphones", "Keyboard", "Mouse"]
+product_prices = [1200, 600, 150, 80, 30]
+
+for i in range(len(product_names)):
+    print(f"Product Name: {product_names[i]}, Price: ${product_prices[i]}")
