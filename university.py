f881832755d94401a24de0dac46379ac987468bd
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 21:53:15 +0800

    feat: adding a famous university

---
+++ university.py
@@ -0,0 +1,5 @@
+university_names = ["Harvard University", "Stanford University", "MIT", "Oxford University", "Cambridge University"]
+university_locations = ["Cambridge, Massachusetts", "Stanford, California", "Cambridge, Massachusetts", "Oxford, England", "Cambridge, England"]
+
+for i in range(len(university_names)):
+    print(f"University Name: {university_names[i]}, Location: {university_locations[i]}")
