f98e6ed811f4db9f655f52810ba4d33eff902500
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 21:54:59 +0800

    feat: adding a good restaurant

---
+++ restaurant.py
@@ -0,0 +1,5 @@
+restaurant_names = ["The Italian Restaurant", "Sushi House", "Burger King", "Taco Bell", "McDonald's"]
+restaurant_cuisine_types = ["Italian", "Japanese", "American", "Mexican", "American"]
+
+for i in range(len(restaurant_names)):
+    print(f"Restaurant Name: {restaurant_names[i]}, Cuisine Type: {restaurant_cuisine_types[i]}")
