c7740869f6db54b69f5319062b9343c18505a5c6
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 21:50:42 +0800

    feat: adding a book titles

---
+++ book.py
@@ -0,0 +1,5 @@
+book_titles = ["The Lord of the Rings", "Harry Potter and the Sorcerer's Stone", "Pride and Prejudice", "To Kill a Mockingbird", "1984"]
+book_authors = ["J.R.R. Tolkien", "J.K. Rowling", "Jane Austen", "Harper Lee", "George Orwell"]
+
+for i in range(len(book_titles)):
+    print(f"Book Title: {book_titles[i]}, Author: {book_authors[i]}")
